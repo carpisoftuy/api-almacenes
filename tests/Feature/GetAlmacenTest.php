f77abb805f3f1c93e5e66c8c66e4a814f88ed8c6
sosam1 <sosam1763@gmail.com>
2023-10-30 19:29:34 -0300

    getAlmacenesTest

---
+++ tests/Feature/GetAlmacenTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Almacen;
+
+class GetAlmacenTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testGetAlmacenes() {
+        $response = $this->get('/api/v2/almacenes');
+        $response->assertStatus(200);
+    } 
+
+
+}
