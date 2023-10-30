aed500898d516e79a04ff61a9ca8436231fc16d0
sosam1 <sosam1763@gmail.com>
2023-10-30 19:53:00 -0300

    paquetes tests

--- tests/Feature/DeletePaqueteTest.php
+++ tests/Feature/DeletePaqueteTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Paquete;
 
 class DeletePaqueteTest extends TestCase
 {
@@ -13,10 +14,31 @@ class DeletePaqueteTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
-    {
-        $response = $this->get('/');
+        public function testDeletePaqueteCorrectamente(){
 
+            $paquete = Paquete::create([
+            'volumen' => 10,
+            'peso' => 5,
+        ]);
+    
+        $response = $this->delete("/api/v2/paquetes/{$paquete->id}");
         $response->assertStatus(200);
-    }
+    
+        }
+   
+       public function testDeletePaqueteConIdIncorrecta(){
+   
+           $paquete = Paquete::create([
+              'volumen' => 10,
+              'peso' => 5,
+          ]);
+   
+          $id_incorrecta = "ads";
+      
+          $response = $this->delete("/api/v2/paquetes/{$id_incorrecta}");
+          $response->assertStatus(500);
+   
+          $paquete->delete();
+   
+          }
 }
